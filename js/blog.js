b8f8250a67ef0e848157aa993c48e39a47f2d288
Yevrey <boyarskiykirill1@gmail.com>
2026-05-27 00:20:37 +0300

    feat: add blog.js

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,70 @@
 //  ====== blog.html ======
 const blogContainer = document.querySelector('.blog__container')
-const sortBtn = document.querySelector('.sort-btn')
-const navLinks = document.querySelectorAll('.header__link')
+
+let blog = [
+	{
+		id: 1,
+		title: 'How I Started My Frontend Journey',
+		date: '15 Mar 2026',
+		description:
+			'I started learning frontend development with HTML and CSS, focusing onunderstanding structure, semanticbasic styling. Over time,I began using FlSCSS, and BEM methoto write cleaner and morestructured code.projects helped meconfidence and betterunderstand howebsites are created.',
+	},
+	{
+		id: 2,
+		title: 'Learning Layouts and Responsive Design',
+		date: '22 Mar 2026',
+		description:
+			'One of the key skills I focused on was responsive layout design. I practiced building inter faceadapt to different sizes using Flexbox,media queriemobile-first princThis helped me undehowto create layoutremain readabfunctional on any device.',
+	},
+	{
+		id: 3,
+		title: 'Common Mistakes and How I Fix Them',
+		date: '28 Mar 2026',
+		description:
+			'While working on my projects, I encountered many common beginner mistakes:improper class nduplicated stylelayout issues. By reviewingmy code and refacit, I learned how tocleaner CSS, structurefiles properly, andrepeating the same in future projects.',
+	},
+	{
+		id: 4,
+		title: 'My Goals as a Junior Frontend Developer',
+		date: '02 Apr 2026',
+		description:
+			'My current goal is to strengthen my frontend fundamentals and move toward JavaScript and frameworks. I aim tomore real-world projects,improve my problem-sskills, anexperience working withcomponent-based inteand interactive features.',
+	},
+]
+
+// ===== CARD TEMPLATE =====
+function createBlogCard(item) {
+	return `
+		<div class="blog__post">
+
+			<div class="blog__content">
+				<h2 class="blog__title">${item.title}</h2>
+
+				<div class="blog__date">
+					${item.date}
+				</div>
+
+				<p class="blog__text">
+					${item.description || ''}
+				</p>
+			</div>
+		</div>
+	`
+}
+
+// ===== RENDER CARDS =====
+function renderBlog() {
+	if (!blogContainer) return
+
+	blogContainer.innerHTML = blog.map(createBlogCard).join('')
+}
+
+renderBlog()
+
+blogContainer.addEventListener('click', e => {
+	const cardBtn = e.target.closest('.blog__btn')
+	if (!cardBtn) return
+	const card = cardBtn.closest('.blog__container')
+	const title = card.querySelector('.blog__title').textContent
+	alert(`You clicked on ${title}`)
+})
