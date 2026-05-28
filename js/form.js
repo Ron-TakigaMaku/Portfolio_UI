78008b10c5112676b778d8fdf23f02494230374c
Yevrey <boyarskiykirill1@gmail.com>
2026-05-28 20:31:23 +0300

    feat: add function in form.js , add contacts in contacts.scss

--- js/form.js
+++ js/form.js
@@ -1,6 +1,47 @@
 // ====== contacts.html ======
-const form = document.querySelector('.contact__form')
-const emailInput = document.querySelector('.email')
-const messageInput = document.querySelector('.message')
-const sendBtn = document.querySelector('.send-btn')
-const navLinks = document.querySelectorAll('.header__link')
+
+const contactsList = document.querySelector('.contacts__list')
+
+let contacts = [
+	{
+		id: 1,
+		email: 'lorem@gmail.com',
+		github: 'Ron-TakigaMaku',
+		location: 'Ukraine',
+	},
+]
+
+// ===== CARD TEMPLATE =====
+
+function createContactsCard(item) {
+	return `
+		<div class="contact-info__list">
+
+			<div class="contact-info__item">
+				<h3>Email</h3>
+				<p>${item.email}</p>
+			</div>
+
+			<div class="contact-info__item">
+				<h3>GitHub</h3>
+				<p>${item.github}</p>
+			</div>
+
+			<div class="contact-info__item">
+				<h3>Location</h3>
+				<p>${item.location}</p>
+			</div>
+
+		</div>
+	`
+}
+
+// ===== RENDER CARDS =====
+
+function renderContacts() {
+	if (!contactsList) return
+
+	contactsList.innerHTML = contacts.map(createContactsCard).join('')
+}
+
+renderContacts()
