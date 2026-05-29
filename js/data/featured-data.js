54291740b5e39f262c9507355db6249bce28f26c
Yevrey <boyarskiykirill1@gmail.com>
2026-05-29 20:12:15 +0300

    refactor folder js

---
+++ js/data/featured-data.js
@@ -0,0 +1,26 @@
+export const featured = [
+	{
+		id: 1,
+		title: 'Trainee frontend',
+		date: '21 Feb 2026',
+		description:
+			'A modern dashboard interface designed with a focus on clarity, hierarchy, and responsiveness...',
+		img: 'img/about__img1.png',
+	},
+	{
+		id: 2,
+		title: 'Trainee frontend',
+		date: '26 Feb 2026',
+		description:
+			'This project showcases my frontend skills in layout building and visual consistency...',
+		img: 'img/about__img2.png',
+	},
+	{
+		id: 3,
+		title: 'Trainee frontend',
+		date: '27 Feb 2026',
+		description:
+			'This project showcases my frontend skills in layout building and visual consistency...',
+		img: 'img/about_img3.png',
+	},
+]
