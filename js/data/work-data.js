ce8ea6620c43d2211f3c6bdb865c04784dc4506a
Yevrey <boyarskiykirill1@gmail.com>
2026-05-31 17:10:15 +0300

    feat:new data category

--- js/data/work-data.js
+++ js/data/work-data.js
@@ -1,32 +1,36 @@
 export const works = [
 	{
 		id: 1,
-		title: 'Trainee frontend',
+		title: 'CRM Dashboard Panel',
 		date: ' 21 Feb 2026',
+		category: ' dashboard',
 		description:
 			'A modern dashboard interface designed with a focus on clarity,hierarchy, and responsiveness. The project demonstrates my ability to structure layouts, work with UI components, and adapt designs for different screen sizes.',
 		img: '../img/about__img1.png',
 	},
 	{
 		id: 2,
-		title: 'Trainee frontend',
+		title: 'Product Landing',
 		date: ' 26 Feb 2026',
+		category: ' landing',
 		description:
 			'This project showcases my frontend skills in layout building andvisual consistency.special attention to spacing,typography, and resbehavior across devices.',
 		img: '../img/about__img2.png',
 	},
 	{
 		id: 3,
-		title: 'Trainee frontend',
+		title: 'Market Place',
 		date: ' 27 Feb 2026',
+		category: 'e-commerce',
 		description:
 			'This project showcases my frontend skills in layout building andvisual consistency.special attention to spacing,typography, and resbehavior across devices.',
 		img: '../img/work__img3.png',
 	},
 	{
 		id: 4,
-		title: 'Trainee frontend',
+		title: 'Creative Agency Portfolio',
 		date: ' 27 Feb 2026',
+		category: 'portfolio',
 		description:
 			'This project showcases my frontend skills in layout building andvisual consistency.special attention to spacing,typography, and resbehavior across devices.',
 		img: '../img/hero__img4.png',
